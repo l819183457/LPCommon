f77c4d10571a52ecd92ae649ddcceaf7b7c10c46
ziqiang.liang <ziqiang.liang@ehking.com>
2018-11-20 17:21:39 +0800

    提交代码

--- AppDelegate.m
+++ AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
-
+#import "NSData+EHKAES.h"
+#import "EHKBase64.h"
 @interface AppDelegate ()
 
 @end
@@ -20,32 +21,85 @@
     return YES;
 }
 
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+//补充字符串
+-(NSMutableString *)FillInString:(NSString *)str {
+    NSInteger length = str.length % 16;
+    NSMutableString * tempStr = [NSMutableString stringWithString:str];
+    if (length == 0) {
+        return tempStr;
+    }
+    for (int i = 0; i<(16 - length); i++) {
+        [tempStr appendString:@"\0"];
+    }
+    return tempStr;
 }
 
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+//文件处理
+-(void)handleFileTxt{
+    //获取需要处理的文件
+    NSString * fileStr = [[NSBundle mainBundle] pathForResource:@"sss" ofType:@"txt"];
+    NSData *data = [NSData alloc]initWithContentsOfFile:fileStr];
+    //字符串处理
+    NSString *readStr =[[NSString alloc]initWithData:data encoding:NSASCIIStringEncoding];
+    //空格分行
+    NSArray * arr = [readStr componentsSeparatedByString:@"\n"];
+    NSMutableArray * totDic = [NSMutableArray array];
+    //便利
+    for ( int i = 0; i<arr.count; i++) {
+        NSString * str = arr [i];
+        //如果这个行为空为无意义的行 读取下一行
+        if ([str isEqualToString: @""]) {
+            continue;
+        }
+        //分割
+        NSArray * arr1 = [str componentsSeparatedByString:@"  "];
+        //标记只取三次
+        int t = 0  ;
+        NSMutableDictionary * subDic = [NSMutableDictionary dictionary];
+        for (int j = 0; j<arr1.count; j++) {
+            NSString * str1 = arr1 [j];
+            //如果这个str为空为无意义的行 读取下一个
+            if ([str1 isEqualToString: @""]) {
+                continue;
+            }
+            //设置当前key
+            NSString * key = @"sss";
+            //判断当前便利的的key值
+            if (t ==0) {
+                key = @"cn_name";
+            } else if (t ==1) {
+                key = @"code";
+            }
+            else if (t ==2) {
+                key = @"name";
+            }
+            else if (t >=3) {
+                continue;
+            }
+            t++;
+            [subDic setObject:str1 forKey:key];
+        }
+        [totDic addObject:subDic];
+        
+    }
+    //保存本地
+    //todo 保存到项目里面
+    [totDic writeToFile:@"/Users/lzq/Desktop/heh.plist" atomically:YES];
 }
 
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+-(void)jiemi {
+    NSData *aes_data = [@"GwEzwk/2GvMjWsKmEbNCCJ7fjwPsaWddcFIWOnquaZ1hSx/JXqPUIv9XNVjib67HcW/RG626omXhqsdbOWFwfgQtiAweNcMnLhBpeems6T0VsFavoMeDwi61UINolTyOnZZbqLxFBp1NuMASi+6kcFT+vCdASPYLlEHw5ZRonPP3Uh9spKWfZZ9DSnO6+r6fI7dSabHZetOPMK5eOa3l6ajYqBnGOdxwJeyGj1cp1AW7PVWG2qoJNvc8XgX1yR5ShEsujsNB8qKaRHv18qbvmZfpPrztPy2GxV+Cs8WQBk4=" base64DecodedData] ;
+    NSString * key = [self buwei:@"papaya123!papaya"];
+    
+    NSString *decryptionString = [[NSString alloc] initWithData:[aes_data AES256_Decrypt:key] encoding:NSUTF8StringEncoding];
+    
+    
 }
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+-(void)jiami    {
+    NSString *aesString = [self buwei:@"39a5995cf7ca9031bc17a7dd2402fd4f"];
+    NSString *key = [self buwei:@"papaya123!@#$papya321$#@!"];
+    NSData * data = [aesString dataUsingEncoding:NSUTF8StringEncoding];
+    NSData * data22 = [data AES256_Encrypt:key];
+    NSString * str =   [data22 newStringInBase64FromData];
 }
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
-
 @end
